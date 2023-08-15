2019c9f73fcafcdcd95577cf7abf986548936593
Zohreh Ashtarilarki <Zohrehashtari.144@gmail.com>
2023-08-14 17:55:03 -0700

    Created about and contact static pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,15 +9,22 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
+    path(route='', view=views.get_dealerships, name='index'),
+
     # path for about view
+    path(route='about/', view=views.about, name='about'),
 
     # path for contact us view
-
+    path(route='contact/', view=views.contact, name='contact'),
+    
     # path for registration
-
+    #path(route='', view=views.registration_request, name='register'),
+    
     # path for login
+    #path(route='', view=views.login_request, name='login'),
 
     # path for logout
+    #path(route='', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
