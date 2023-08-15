b6014a33ee37ef17fa6ed7de94eb70b2bf5db244
Zohreh Ashtarilarki <Zohrehashtari.144@gmail.com>
2023-08-15 14:02:11 -0700

    Debugged main.py based on Lint Code

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -1,19 +1,18 @@
-#
-#
-# main() will be run when you invoke this action
-#
-# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-#
-# @return The output of this action, which must be a JSON object.
-#
-#
 from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 import requests
 
-
 def main(dict):
-    databaseName = "dealerships"
+    """
+    This is the main function that will be run when you invoke this action.
+
+    Args:
+        dict (dict): Cloud Functions actions accept a single parameter, which must be a JSON object.
+
+    Returns:
+        dict: The output of this action, which must be a JSON object.
+    """
+    database_name = "dealerships"  # Renamed the variable to follow snake_case convention
 
     try:
         client = Cloudant.iam(
@@ -23,10 +22,10 @@ def main(dict):
         )
         print("Databases: {0}".format(client.all_dbs()))
     except CloudantException as ce:
-        print("unable to connect")
-        return {"error": ce}
+        print("Unable to connect")  # Corrected the capitalization
+        return {"error": str(ce)}  # Convert CloudantException to string
     except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
+        print("Connection error")
+        return {"error": str(err)}  # Convert the exception to string
 
-    return {"dbs": client.all_dbs()}
+    return {"dbs": client.all_dbs()}  # Return a JSON response
