3dd0b04c597ef4e531d0518d74358838159c16c4
Zohreh Ashtarilarki <Zohrehashtari.144@gmail.com>
2023-08-15 14:17:08 -0700

    Debugged main.py based on Lint Code

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -1,32 +1,41 @@
+"""
+This module contains the main function for the Cloud Functions action.
+
+It connects to a Cloudant database and retrieves information.
+
+Usage:
+    - This module is invoked when the Cloud Function is triggered.
+"""
+
 from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 import requests
 
-def main(dict):
+def main(input_dict):
     """
     This is the main function that will be run when you invoke this action.
 
     Args:
-        dict (dict): Cloud Functions actions accept a single parameter, which must be a JSON object.
+        input_dict (dict): Cloud Functions actions accept a single parameter, which must be a JSON object.
 
     Returns:
         dict: The output of this action, which must be a JSON object.
     """
     try:
         client = Cloudant.iam(
-            account_name=dict["COUCH_USERNAME"],
-            api_key=dict["IAM_API_KEY"],
+            account_name=input_dict["COUCH_USERNAME"],
+            api_key=input_dict["IAM_API_KEY"],
             connect=True,
         )
-        print("Databases:", client.all_dbs())  # Used an f-string for formatting
-    except CloudantException as ce:
+        print("Databases:", client.all_dbs())
+    except CloudantException as cloudant_exception:
         print("Unable to connect")
-        return {"error": str(ce)}
+        return {"error": str(cloudant_exception)}
     except (requests.exceptions.RequestException, ConnectionResetError) as err:
         print("Connection error")
         return {"error": str(err)}
 
-    return {"dbs": client.all_dbs()}  # Return a JSON response
+    return {"dbs": client.all_dbs()}
 
 if __name__ == "__main__":
     pass  # Placeholder to satisfy the missing module docstring warning
