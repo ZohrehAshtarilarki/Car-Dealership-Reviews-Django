8a4e9b1dda4fc03cf603f51ea159696ce347db4a
Zohreh Ashtarilarki <Zohrehashtari.144@gmail.com>
2023-08-15 14:29:35 -0700

    Debugged main.py based on Lint Code

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -1,10 +1,10 @@
 """
-This module contains the main function for the Cloud Functions action.
-
+This module contains the main function for the Cloud Functions 
+action.
 It connects to a Cloudant database and retrieves information.
-
 Usage:
-    - This module is invoked when the Cloud Function is triggered.
+    - This module is invoked when the Cloud Function is 
+    triggered.
 """
 
 from cloudant.client import Cloudant
@@ -13,13 +13,16 @@ import requests
 
 def main(input_dict):
     """
-    This is the main function that will be run when you invoke this action.
+    This is the main function that will be run when you 
+    invoke this action.
 
     Args:
-        input_dict (dict): Cloud Functions actions accept a single parameter, which must be a JSON object.
+        input_dict (dict): Cloud Functions actions accept a 
+        single parameter, which must be a JSON object.
 
     Returns:
-        dict: The output of this action, which must be a JSON object.
+        dict: The output of this action, which must be a JSON 
+        object.
     """
     try:
         client = Cloudant.iam(
@@ -31,11 +34,13 @@ def main(input_dict):
     except CloudantException as cloudant_exception:
         print("Unable to connect")
         return {"error": str(cloudant_exception)}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+    except (requests.exceptions.RequestException, 
+            ConnectionResetError) as err:
         print("Connection error")
         return {"error": str(err)}
 
     return {"dbs": client.all_dbs()}
 
 if __name__ == "__main__":
-    pass  # Placeholder to satisfy the missing module docstring warning
+    pass  # Placeholder to satisfy the missing module 
+          # docstring warning
